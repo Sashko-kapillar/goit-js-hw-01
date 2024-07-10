1a313b8a4ce35f1f241ac832af00b942e9a744c3
sashko-kapillar <kapillarua@gmail.com>
2024-07-10 17:31:39 +0300

    change

--- js/task-3.js
+++ js/task-3.js
@@ -21,9 +21,9 @@ function getElementWidth(content, padding, border) {
 }
 
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+// console.log(getElementWidth("50px", "8px", "4px")); // 74
+// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+// console.log(getElementWidth("200px", "0px", "0px")); // 200
 
 // let g = "50px";
 // if (typeof g === "string") {
