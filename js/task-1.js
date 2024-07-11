9b2033fb834534bf7c5be9edf0068ded0f42eda6
sashko-kapillar <kapillarua@gmail.com>
2024-07-11 09:44:44 +0300

    wriht const

--- js/task-1.js
+++ js/task-1.js
@@ -5,9 +5,9 @@ function makeTransaction(quantity, pricePerDroid) {
   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
-// console.log(makeTransaction(5, 3000)); 
-// console.log(makeTransaction(3, 1000)); 
-// console.log(makeTransaction(10, 500));
+console.log(makeTransaction(5, 3000)); 
+console.log(makeTransaction(3, 1000)); 
+console.log(makeTransaction(10, 500));
 
 // const age = 20;
 // console.log(age);
